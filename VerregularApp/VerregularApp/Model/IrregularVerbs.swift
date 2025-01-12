44c1c054a562a87bf0402e356dab0e6d289da6e7
IvanKeyiOS <ivanbeat888@gmail.com>
2025-01-12 14:32:48 +0100

    working with background and adding verbs

--- VerregularApp/VerregularApp/Model/IrregularVerbs.swift
+++ VerregularApp/VerregularApp/Model/IrregularVerbs.swift
@@ -13,11 +13,23 @@ class IrregularVerbs {
      
     func configureVerbs() {
         verbs = [
+            Verb(infinitive: "be", pastSimple: "was/were", participle: "been"),
+            Verb(infinitive: "become", pastSimple: "became", participle: "become"),
+            Verb(infinitive: "begin", pastSimple: "began", participle: "begun"),
             Verb(infinitive: "blow", pastSimple: "blew", participle: "blown"),
+            Verb(infinitive: "break", pastSimple: "broke", participle: "broken"),
+            Verb(infinitive: "choose", pastSimple: "chose", participle: "chosen"),
             Verb(infinitive: "draw", pastSimple: "drew", participle: "drawn"),
+            Verb(infinitive: "drink", pastSimple: "drank", participle: "drunk"),
             Verb(infinitive: "eat", pastSimple: "ate", participle: "eaten"),
-            Verb(infinitive: "fall", pastSimple: "fell", participle: "fallen")
-           
+            Verb(infinitive: "fall", pastSimple: "fell", participle: "fallen"),
+            Verb(infinitive: "fly", pastSimple: "flew", participle: "flown"),
+            Verb(infinitive: "give", pastSimple: "gave", participle: "given"),
+            Verb(infinitive: "go", pastSimple: "went", participle: "gone"),
+            Verb(infinitive: "grow", pastSimple: "grew", participle: "grown"),
+            Verb(infinitive: "have", pastSimple: "had", participle: "had"),
+            Verb(infinitive: "know", pastSimple: "knew", participle: "known")
+            
         ]
     }
 }
